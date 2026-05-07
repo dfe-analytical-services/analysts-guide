38ed435ee27f13ec71dd925ea5794f04e5da3dd5
Menna <menna.zayed@education.gov.uk>
2026-05-07 08:53:39 +0100

    redoing the snapshot (#257)

--- renv/activate.R
+++ renv/activate.R
@@ -3,7 +3,7 @@ local({
 
   # the requested version of renv
   version <- "1.2.2"
-  attr(version, "md5") <- ..md5..
+  attr(version, "md5") <- "bb69b6403b1bad0442657e9e8e57cc83"
   attr(version, "sha") <- NULL
 
   # the project directory
